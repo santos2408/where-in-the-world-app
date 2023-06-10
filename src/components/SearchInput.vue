b3df522d0c63c083d1e67fd25c949babd209ac2e
Roger Santos <roger.santos36@gmail.com>
2023-06-10 00:43:42 -0300

    created search input component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <label
+      for="search-country"
+      class="flex h-14 w-120 items-center gap-6 rounded bg-brand-white-1 px-8 text-brand-gray-1 shadow-dark-2 transition ease-in dark:bg-brand-dark-1 dark:text-brand-white-1"
+    >
+      <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+      <input
+        id="search-country"
+        type="text"
+        class="h-full w-full bg-transparent outline-none transition ease-in"
+        placeholder="Search for a country..."
+      />
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchInput",
+};
+</script>
