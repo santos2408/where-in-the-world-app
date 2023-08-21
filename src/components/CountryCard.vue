3e501f7a124a6ab68cc4f56e48ff1115d48c3cf4
Roger Santos <roger.santos36@gmail.com>
2023-08-21 17:31:00 -0300

    adjusts

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="overflow-hidden rounded bg-brand-white-1 shadow-dark-2">
+    <div>
+      <img
+        class="h-52 object-cover"
+        :src="`https://flagcdn.com/${sigle.toLowerCase()}.svg`"
+        alt="Brazil"
+      />
+    </div>
+    <div class="p-6">
+      <h2 class="mb-6 text-lg font-bold">{{ country }}</h2>
+      <ul class="space-y-1">
+        <li><span class="font-semibold">Population</span>: {{ population }}</li>
+        <li><span class="font-semibold">Region</span>: {{ region }}</li>
+        <li><span class="font-semibold">Capital</span>: {{ capital }}</li>
+        <li><span class="font-semibold"></span>{{ sigle }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CountryCard",
+  props: ["country", "population", "region", "capital", "sigle"],
+};
+</script>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -2,24 +2,48 @@
   <main
     class="w- min-h-screen bg-brand-white-2 px-20 pt-32 transition ease-in dark:bg-brand-dark-2"
   >
-    <section class="flex items-center justify-between">
+    <section class="mb-12 flex items-center justify-between">
       <search-input />
       <location-filter />
     </section>
 
-    <section></section>
+    <section class="grid grid-cols-5 gap-16">
+      <country-card
+        v-for="country in countries"
+        :key="country"
+        :country="country.name.common"
+        :population="country.population"
+        :region="country.region"
+        :capital="country.capital"
+        :sigle="country.cca2"
+      />
+    </section>
   </main>
 </template>
 
 <script>
 import SearchInput from "@/components/SearchInput.vue";
 import LocationFilter from "@/components/LocationFilter.vue";
+import CountryCard from "@/components/CountryCard.vue";
 
 export default {
   name: "MainSection",
   components: {
     LocationFilter,
     SearchInput,
+    CountryCard,
+  },
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  created() {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((countries) => {
+        this.countries = countries;
+      });
   },
 };
 </script>
