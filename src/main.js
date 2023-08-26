1b245fbc6ac5868c57c9834e12e9c5df2a9a95f4
Roger Santos <roger.santos36@gmail.com>
2023-08-26 15:16:46 -0300

    setup pinia in main js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import router from "@/router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,9 +10,12 @@ import { faSun } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faMoon, faMagnifyingGlass, faSun, faAngleDown);
-
 import "@/index.css";
 import App from "@/App.vue";
 
-createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+library.add(faMoon, faMagnifyingGlass, faSun, faAngleDown);
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
