b0b633ea183b687554ef5cf746de1bef25ea120d
Roger Santos <roger.santos36@gmail.com>
2023-08-29 19:47:10 -0300

    refactor: filter region menu exibition and added transition in arrow filter icon

--- src/components/LocationFilter.vue
+++ src/components/LocationFilter.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="relative z-10">
     <div
-      class="relative flex h-14 w-60 cursor-pointer items-center justify-between gap-14 rounded bg-brand-white-1 px-6 text-brand-gray-1 shadow-dark-2 dark:bg-brand-dark-1 dark:text-brand-white-1"
+      class="relative flex h-14 w-60 cursor-pointer items-center justify-between gap-14 rounded bg-brand-white-1 px-6 text-brand-gray-1 shadow-dark-2 hover:bg-brand-gray-2 dark:bg-brand-dark-1 dark:text-brand-white-1 dark:hover:bg-brand-dark-3"
       @click="handleFilterRegionsVisibility"
     >
       <span v-if="selectedRegion">{{ selectedRegion }}</span>
       <span v-else>Filter by region</span>
-      <font-awesome-icon :icon="['fas', 'angle-down']" class="" />
+      <font-awesome-icon
+        :icon="['fas', 'angle-down']"
+        :class="['transition', { '-rotate-180': filterRegionsVisibility }]"
+      />
     </div>
 
     <ul
-      v-if="filterRegionsVisibility"
-      class="absolute left-0 top-16 flex w-full flex-col rounded bg-brand-white-1 text-brand-gray-1 shadow-dark-2 dark:bg-brand-dark-1 dark:text-brand-white-1"
+      v-show="filterRegionsVisibility"
+      class="absolute left-0 top-16 flex w-full flex-col rounded bg-brand-white-1 text-brand-gray-1 shadow-dark-2 transition dark:bg-brand-dark-1 dark:text-brand-white-1"
     >
-      <template v-for="region in regions">
+      <template v-for="region in regions" :key="region">
         <li
           v-if="region !== selectedRegion"
-          :key="region"
           class="cursor-pointer px-6 py-3 hover:bg-brand-gray-2 dark:hover:bg-brand-dark-3"
           @click="chooseRegion"
         >
