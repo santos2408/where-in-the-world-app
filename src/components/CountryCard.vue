8e9c1390645f2348f54280551c1b96079c9bcacb
Roger Santos <roger.santos36@gmail.com>
2023-08-29 19:46:18 -0300

    feat: formatted population unit number

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="overflow-hidden rounded bg-white shadow-dark-2">
+  <div
+    class="cursor-pointer overflow-hidden rounded bg-white shadow-dark-2 transition hover:-translate-y-1"
+  >
     <figure class="flex h-60 items-center justify-center p-6" :style="backgroundStyleCountry">
       <img
         class="max-h-52 w-64 border-2 border-white object-cover drop-shadow-lg backdrop-blur"
@@ -10,7 +12,7 @@
     <div class="bg-brand-white-1 p-6">
       <h2 class="mb-6 text-lg font-bold">{{ country.name.common }}</h2>
       <ul class="space-y-1">
-        <li><span class="font-semibold">Population</span>: {{ country.population }}</li>
+        <li><span class="font-semibold">Population</span>: {{ formattedPopulation }}</li>
         <li><span class="font-semibold">Region</span>: {{ country.region }}</li>
         <li><span class="font-semibold">Capital</span>: {{ formattedCountryCapital }}</li>
       </ul>
@@ -36,6 +38,9 @@ export default {
     },
     backgroundStyleCountry() {
       return { backgroundColor: `rgb(${this.countryColor.join(",")}, 0.5)` };
+    },
+    formattedPopulation() {
+      return new Intl.NumberFormat("en-US").format(this.country.population);
     },
   },
 };
